efa6de603b2ce43ec42d9aca7f7d60006abcda32
Vyacheslav Targoniy <huntersyava@gmail.com>
2013-02-04 22:46:30 +0400

    try configure hibernate

--- src/main/java/com/in6k/employees/db/HibernateUtil.java
+++ src/main/java/com/in6k/employees/db/HibernateUtil.java
@@ -5,6 +5,8 @@ import com.in6k.employees.domain.EmployeeModel;
 import org.hibernate.*;
 import org.hibernate.cfg.*;
 
+import java.io.File;
+
 public class HibernateUtil {
     public static void main(String[] args) {
         EmployeeModel employeeModel = new EmployeeModel();
@@ -14,9 +16,10 @@ public class HibernateUtil {
         employeeModel.setPassword("123");
         employeeModel.setBirhdate("12-11-1991");
 
-        Configuration configuration = new Configuration();
+//        Configuration configuration = new Configuration();
+//        File f =new File(".");
 
-//        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
 //        Session session = sessionFactory.openSession();
 //        session.save(employeeModel);
     }
