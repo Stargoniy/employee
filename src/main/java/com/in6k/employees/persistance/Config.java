1114332d4fb635579f7da4fbaaea8ab4385c53cd
Vyacheslav Targoniy <huntersyava@gmai>
2013-02-04 09:16:29 +0200

    fix config

--- src/main/java/com/in6k/employees/persistance/Config.java
+++ src/main/java/com/in6k/employees/persistance/Config.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class Config {
     private static final String WINDOWS_DIR = "F:\\Projects\\IdeaProjects\\employee\\users\\";
-    private static final String LINUX_DIR = "/home/employee/IdeaProjects/employees/users/";
+    private static final String LINUX_DIR = "/home/employee/IdeaProjects/employee/users/";
 
     public static String getUsersDir() {
         if (new File(LINUX_DIR).exists()) {
